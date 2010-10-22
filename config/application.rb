76903c9f29a54e1215ae740731fdbc004aeccf5e
Reid Beels <mail@reidbeels.com>
2010-10-22 16:25:47 -0700

    don't autoload from plugins, please

--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module Citizenry
     # Custom directories with classes and modules you want to be autoloadable.
     # config.autoload_paths += %W(#{config.root}/extras)
 
+    config.eager_load_paths = ['app/models', 'app/controllers', 'app/helpers']     
+
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
     # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
