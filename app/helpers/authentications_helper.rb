b87e9cc5d5104eaeec3e74ef1536298d2299d28c
Reid Beels <mail@reidbeels.com>
2011-01-23 21:03:17 -0800

    Added OmniAuth service icons from https://github.com/intridea/authbuttons

--- app/helpers/authentications_helper.rb
+++ app/helpers/authentications_helper.rb
@@ -1,2 +1,12 @@
 module AuthenticationsHelper
+  def provider_icon(provider, size=32)
+    path = File.join('authbuttons',"#{provider}_#{size}.png")
+    provider_name = OmniAuth::Utils.camelize(provider)
+
+    if File.exists?(Rails.root.join('public', 'images', path))
+      image_tag(path, :alt => provider_name)
+    else
+      provider_name
+    end
+  end
 end
