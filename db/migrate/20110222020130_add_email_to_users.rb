4d1c5d933f48e76d8264aa297a2aab1d8ae8be18
Reid Beels <mail@reidbeels.com>
2011-02-21 20:44:34 -0800

    [migration] Added email field to users

---
+++ db/migrate/20110222020130_add_email_to_users.rb
@@ -0,0 +1,9 @@
+class AddEmailToUsers < ActiveRecord::Migration
+  def self.up
+    add_column :users, :email, :string
+  end
+
+  def self.down
+    remove_column :users, :email
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110123235946) do
+ActiveRecord::Schema.define(:version => 20110222020130) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -132,6 +132,7 @@ ActiveRecord::Schema.define(:version => 20110123235946) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin",               :default => false
+    t.string   "email"
   end
 
 end
