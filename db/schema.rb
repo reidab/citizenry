f9211f27e29566d067127dd2a9c6d79f560717cf
Igal Koshevoy <igal@pragmaticraft.com>
2011-02-27 11:29:03 -0800

    MIGRATION: Add 'paper_trail' gem's tables.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110228031139) do
+ActiveRecord::Schema.define(:version => 20110227012345) do
 
   create_table "authentications", :force => true do |t|
     t.integer  "user_id"
@@ -153,5 +153,16 @@ ActiveRecord::Schema.define(:version => 20110228031139) do
     t.boolean  "admin",               :default => false
     t.string   "email"
   end
+
+  create_table "versions", :force => true do |t|
+    t.string   "item_type",  :null => false
+    t.integer  "item_id",    :null => false
+    t.string   "event",      :null => false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], :name => "index_versions_on_item_type_and_item_id"
 
 end
