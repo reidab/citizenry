1e153d83b45545da5d5e1acd50c667ac47fc6843
Reid Beels <mail@reidbeels.com>
2011-02-28 23:10:17 -0800

    Styled flash message display, since they kind of contain important information.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -21,4 +21,38 @@ module ApplicationHelper
   def provider_name(provider)
     OmniAuth::Utils.camelize(provider)
   end
+
+  def render_flash
+    unless flash.blank?
+      content_tag("div", :id => "flash", :class => "flash") do
+        flash.keys.sort{|a,b| a.to_s <=> b.to_s }.map do |key|
+          render_message(key, flash[key])
+        end.join("\n").html_safe
+      end
+    end
+  end
+
+  def render_message(type, content, label = true)
+    classes = ['message', type, (label ? 'with_label' : nil)]
+    title = flash_type_title(type)
+    content_tag("div", :class => classes.compact.join(' ')) do
+      (label ? content_tag(:span, title, :class => 'message_type') : '') << content_tag(:div, content, :class => 'message_content')
+    end
+  end
+
+  def flash_type_title(type)
+    flowery_vocabulary = {
+      :error   => [ 'Egads', 'Balderdash', 'Fiddlesticks', 'Holy Toledo', 'Tarnation', 'Damnation', 'Fooey',
+                    'Boo', 'Aw shucks', 'Uh oh', 'Great Scott', "Blisterin' barnacles" ],
+      :success => [ 'Yippee', 'Hooray', 'Awesome', 'Yeehaw', 'Hoorah', 'Huzzah', 'Yeah', 'Yay' ],
+      :notice  => [ 'Heads up', 'Avast', 'Notice' ],
+      :warning => [ 'Beware', 'Warning', 'Watch out' ]
+    }
+
+    if flowery_vocabulary[type]
+      return "#{flowery_vocabulary[type].choice}!"
+    else
+      return type.to_s.titleize
+    end
+  end
 end
