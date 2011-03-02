c2ec171b7a2b2cdf69acbe24143322ac4dea78ee
Reid Beels <mail@reidbeels.com>
2011-03-02 14:51:41 -0800

    Temporarily unprotect Person#imported_from_provider and imported_from_id

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,5 +1,5 @@
 class Person < ActiveRecord::Base
-  attr_protected :user_id, :imported_from_provider, :imported_from_id
+  attr_protected :user_id
   has_paper_trail :ignore => [:user_id]
 
   require 'open-uri'
