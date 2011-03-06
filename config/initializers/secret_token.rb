e7d7be08d2bb00183db5cb06dbc7824025a7b30a
Igal Koshevoy <igal@pragmaticraft.com>
2011-03-06 05:23:14 -0800

    Fix security of 'secret_token'.
    Previously it was hardcoded into the initializers and publicly available.
    It's now loaded from the private SETTINGS value, and if not available,
    falls back to the previous (and now public) value.

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Citizenry::Application.config.secret_token = 'c63463de2c67e13400b5ee30329a12e6d95df8e7a24112c4420ceb0b540829cdcfa48cd1d54d9dff3cc89c552013cf9f5aab7dd2539291d9355bc2069367bafe'
+Citizenry::Application.config.secret_token = SETTINGS[:secret_token] || 'c63463de2c67e13400b5ee30329a12e6d95df8e7a24112c4420ceb0b540829cdcfa48cd1d54d9dff3cc89c552013cf9f5aab7dd2539291d9355bc2069367bafe'
