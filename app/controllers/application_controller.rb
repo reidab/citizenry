77beab717b7bcbc444daf28455dcb420a6b07f66
Igal Koshevoy <igal@pragmaticraft.com>
2011-03-06 04:04:47 -0800

    Configure responders and flash notifications text.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
+require "application_responder"
+
 class ApplicationController < ActionController::Base
+  self.responder = ApplicationResponder
+  respond_to :html, :json, :xml
+
   protect_from_forgery
 
   rescue_from ActiveRecord::RecordNotFound do |exception|
