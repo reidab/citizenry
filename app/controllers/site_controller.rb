91c093af4838be96f626855611a0a3e3587a9d92
Reid Beels <mail@reidbeels.com>
2011-03-09 19:30:13 -0800

    Show link to calagator on front page if events count exists

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,5 +1,8 @@
 class SiteController < ApplicationController
   def index
-    
+    events_count_path = 'tmp/calagator_events_count'
+    if File.exists?(events_count_path)
+      @events_count = File.read(events_count_path).to_i
+    end
   end
 end
