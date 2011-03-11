a491fff4879e06087794d8a49abe9bf0d911c6e7
Reid Beels <mail@reidbeels.com>
2011-03-10 19:38:25 -0800

    Swipe database dump/restore tools from OCW

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,31 @@ namespace :epdx do
     run "cd #{release_path} && rake RAILS_ENV=#{rails_env} calagator:update_count"
   end
 end
+
+namespace :db do
+  namespace :remote do
+    desc "Dump database on remote server"
+    task :dump, :roles => :db, :only => {:primary => true} do
+      run "(cd #{current_path} && rake RAILS_ENV=production db:raw:dump FILE=#{shared_path}/db/database.sql)"
+    end
+  end
+
+  namespace :local do
+    desc "Restore downloaded database on local server"
+    task :restore, :roles => :db, :only => {:primary => true} do
+      sh "rake db:raw:dump FILE=database~old.sql && rake db:raw:restore FILE=database.sql"
+    end
+  end
+
+  desc "Download database from remote server"
+  task :download, :roles => :db, :only => {:primary => true} do
+    sh "rsync -uvax #{user}@#{host}:#{shared_path}/db/database.sql ."
+  end
+
+  desc "Use: dump and download remote database and restore it locally"
+  task :use, :roles => :db, :only => {:primary => true} do
+    db.remote.dump
+    db.download
+    db.local.restore
+  end
+end
