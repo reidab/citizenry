cf4ff73788aa88924c57b9312af0287903e8a08c
Reid Beels <mail@reidbeels.com>
2011-03-12 01:00:55 -0800

    [gem] Update OmniAuth to version 0.2 and switch to using version 2 of the Foursquare API.

--- Gemfile
+++ Gemfile
@@ -10,7 +10,7 @@ gem 'mysql'
 gem 'formtastic', '~>1.1.0'
 
 gem 'devise', "~> 1.1.5"
-gem 'omniauth', "~> 0.1.6"
+gem 'omniauth', "~> 0.2.0"
 
 gem 'redfinger', '~> 0.1.0', :git => "https://github.com/reidab/redfinger.git"
 
@@ -21,7 +21,7 @@ gem 'net-dns', '~> 0.6.1', :require => 'net/dns/resolver'
 gem 'twitter', '~> 1.1.1'
 gem 'linkedin', '~> 0.1.7', :git => "https://github.com/pengwynn/linkedin.git"
 gem 'mogli', '~>0.0.25', :git => "https://github.com/reidab/mogli.git"
-gem 'foursquare', '~>0.3.4'
+gem 'foursquare2', '~>0.9.0'
 # https://gist.github.com/419219 <- github oauth!
 
 gem "haml", "~> 3.0.18"
--- lib/api_client/foursquare.rb
+++ lib/api_client/foursquare.rb
@@ -1,16 +1,15 @@
 module APIClient
-  class Foursquare < APIClient::OAuth
+  class Foursquare < APIClient::OAuth2
     def initialize(auth, options={})
       super(auth)
-      fs_auth = ::Foursquare::OAuth.new( SETTINGS['auth_credentials']['foursquare']['key'],
-                                         SETTINGS['auth_credentials']['foursquare']['secret'] )
-      fs_auth.authorize_from_access(@auth.access_token, @auth.access_token_secret)
 
-      @client = ::Foursquare::Base.new(fs_auth)
+      # If we have a secret from OAuth1, we can use it here.
+      access_token = @auth.access_token_secret || @auth.access_token
+      @client = Foursquare2::Client.new(:oauth_token => access_token)
     end
 
     def search(query, options = {})
-      @client.findfriends_byname(:q => query, :l => (options[:limit] || DEFAULT_LIMIT)).map{|fs_user|
+      @client.search_users(:name => query).results.map{|fs_user|
         self.person_from(fs_user)
       }
     rescue ::Foursquare::Unauthorized => e
@@ -18,7 +17,7 @@ module APIClient
     end
 
     def get(id)
-      fs_user = @client.user(:uid => id)
+      fs_user = @client.user(id)
       self.person_from(fs_user) if fs_user.present?
     rescue ::Foursquare::Unauthorized => e
       raise APIAuthenticationError, e.inspect
@@ -27,9 +26,9 @@ module APIClient
     def person_from(fs_user)
       fs_user = Hashie::Mash.new(fs_user) if fs_user.class == Hash
 
-      Person.new( :name                   => [fs_user.firstname, fs_user.lastname].join(' '),
+      Person.new( :name                   => [fs_user.firstName, fs_user.lastName].join(' '),
                   :photo_import_url       => fs_user.photo,
-                  :location               => fs_user.homecity ) \
+                  :location               => fs_user.homeCity ) \
                   .tap{|person|
                     person.imported_from_provider = 'foursquare'
                     person.imported_from_id       = fs_user.id
