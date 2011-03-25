acb60366897bd8e66a26e2eeb14a5800da063409
Reid Beels <mail@reidbeels.com>
2011-03-24 21:48:51 -0700

    Updated model annotations

--- app/models/person.rb
+++ app/models/person.rb
@@ -52,25 +52,28 @@ end
 
 
 
+
 # == Schema Information
 #
 # Table name: people
 #
-#  id                     :integer(4)      not null, primary key
-#  email                  :string(255)
-#  twitter                :string(255)
-#  url                    :string(255)
-#  bio                    :text
-#  created_at             :datetime
-#  updated_at             :datetime
-#  user_id                :integer(4)
-#  name                   :string(255)
-#  imported_from_provider :string(255)
-#  imported_from_id       :string(255)
-#  location               :string(255)
-#  photo_file_name        :string(255)
-#  photo_content_type     :string(255)
-#  photo_file_size        :integer(4)
-#  photo_updated_at       :datetime
+#  id                        :integer(4)      not null, primary key
+#  email                     :string(255)
+#  twitter                   :string(255)
+#  url                       :string(255)
+#  bio                       :text
+#  created_at                :datetime
+#  updated_at                :datetime
+#  user_id                   :integer(4)
+#  name                      :string(255)
+#  imported_from_provider    :string(255)
+#  imported_from_id          :string(255)
+#  location                  :string(255)
+#  photo_file_name           :string(255)
+#  photo_content_type        :string(255)
+#  photo_file_size           :integer(4)
+#  photo_updated_at          :datetime
+#  reviewed                  :boolean(1)      default(FALSE)
+#  imported_from_screen_name :string(255)
 #
 
