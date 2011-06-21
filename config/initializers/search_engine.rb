cdfd8af35bf2afc6c07bee7d047e6504a5368ca6
Linus Graybill <graybill@gmail.com>
2011-06-20 21:08:29 -0400

    Hide for heroku

--- config/initializers/search_engine.rb
+++ config/initializers/search_engine.rb
@@ -2,8 +2,8 @@ require 'lib/search_engine'
 SearchEngine.kind = SETTINGS["search_engine"]
 
 # Mention everything to ensure that rails eager loading doesn't get the better of us
-Person
-Company
-Project
-Group
-ResourceLink
+# Person
+# Company
+# Project
+# Group
+# ResourceLink
