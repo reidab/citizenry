4e6ca030d36c26d609866276efdc7bfd53a396df
Linus Graybill <graybill@gmail.com>
2011-07-13 14:14:49 -0400

    Conditional to use RANDOM() for pg

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,7 +25,7 @@ class ApplicationController < ActionController::Base
     seed = session["#{controller_name}_random_sort_seed"] ||= rand(2147483647)
     direction = %w(asc desc).include?(params[:order]) ? params[:order].upcase : ''
 
-    if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'sqlite3'
+    if ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'sqlite3' || ActiveRecord::Base.configurations[Rails.env]['adapter'] == 'postgresql'
       "RANDOM() #{direction}"
     else
       "RAND(#{seed}) #{direction}"
