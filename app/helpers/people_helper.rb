1c2d4ce749b01d885420593ba9ffccaafeda1d38
Reid Beels <mail@reidbeels.com>
2011-07-19 16:59:04 -0700

    Set an appropriate title for the people/index when showing mentors or mentees.

--- app/helpers/people_helper.rb
+++ app/helpers/people_helper.rb
@@ -1,2 +1,14 @@
 module PeopleHelper
+  def people_list_title(list, tag)
+    singular_title = "#{tag} person"
+    if mentoring_enabled?
+      singular_title = "interested mentor" if params[:mentors]
+      singular_title = "interested mentee" if params[:mentees]
+    end
+
+    semantic_pluralize(
+      list.respond_to?(:total_entries) ? list.total_entries : list.size,
+      singular_title.strip.titleize
+    )
+  end
 end
