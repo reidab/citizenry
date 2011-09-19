4cf65bf1ee69a79113b6ed931b185129664169ae
Igal Koshevoy <igal@pragmaticraft.com>
2011-09-18 19:05:01 -0700

    Update acceptance specs to use new #import_image_from_url system.

--- spec/acceptance/groups_spec.rb
+++ spec/acceptance/groups_spec.rb
@@ -259,8 +259,6 @@ feature "The group edit form" do
   end
 
   scenario "should allow a user to import a logo from the web" do
-    pending "not yet implemented"
-
     FakeWeb.register_uri(:get, "http://example.com/photo.png", 
                          :body => File.read(Rails.root.join('spec', 'acceptance', 'support', 'test_photo.png')))
 
