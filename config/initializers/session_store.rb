a4a7d0b621042221bd66be16c848f5562e3f4d43
Reid Beels <mail@reidbeels.com>
2011-09-24 15:56:25 -0700

    Move session cookie key into settings.yml, changed default session cookie key.
    This will invalidate all existing user sessions, which is necessary due to recent changes in devise.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Citizenry::Application.config.session_store :cookie_store, :key => '_citizenry_session'
+Citizenry::Application.config.session_store :cookie_store, :key => SETTINGS[:session_cookie_key] || '_citizenry_app_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
