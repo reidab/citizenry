701c9a6dfd146a728da73954fe3e36edc1d87c51
Marc Chung <mchung@gmail.com>
2012-12-27 00:12:50 -0700

    Style sheets in assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -44,6 +44,7 @@ Citizenry::Application.configure do
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
+  config.assets.precompile += %w( advanced_mobile.css basic_mobile.css desktop.css )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
