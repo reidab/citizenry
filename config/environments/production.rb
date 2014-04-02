282cb118563b648d47e08e5b3699ca904fff66a4
Reid Beels <reid.beels@elementaltechnologies.com>
2014-04-02 10:40:01 -0700

    Silence deprecation warnings in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Citizenry::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb
 
+  ::ActiveSupport::Deprecation.silenced = true
+
   # Code is not reloaded between requests
   config.cache_classes = true
 
