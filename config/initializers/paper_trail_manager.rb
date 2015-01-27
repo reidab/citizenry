9d6109bb819fe78c84f3c1a35dce86a9ac8f806f
Reid Beels <mail@reidbeels.com>
2015-01-26 21:06:32 -0800

    Require admin to view changelog

--- config/initializers/paper_trail_manager.rb
+++ config/initializers/paper_trail_manager.rb
@@ -1,1 +1,16 @@
 PaperTrailManager.whodunnit_class = User
+
+PaperTrailManager.allow_index_when { |c|
+  c.authenticate_user!
+  c.current_user && c.current_user.admin?
+}
+
+PaperTrailManager.allow_show_when { |c|
+  c.authenticate_user!
+  c.current_user && c.current_user.admin?
+}
+
+PaperTrailManager.allow_revert_when { |c|
+  c.authenticate_user!
+  c.current_user && c.current_user.admin?
+}
